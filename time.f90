1f06b9f07168072207887d0756e3ace413208677
Frank S. Tsung <tsung.fs@gmail.com>
2024-10-24 19:36:12 -0700

    changed time.f90
    had to modify time.f90 on the Intel Mac's

--- time.f90
+++ time.f90
@@ -23,7 +23,8 @@ real(kind=p_double) :: get_time_in_scs
 ! number of ticks since a fixed time in the past
 real(kind=p_double), external :: timer_cpu_seconds
 
-get_time_in_scs = timer_cpu_seconds()
+! get_time_in_scs = timer_cpu_seconds()
+get_time_in_scs = 0.0
 
 end function get_time_in_scs
 !---------------------------------------------------
